d3ca133aaf09c7dc98772539056e5db43cbc296c
Polygeekism <irishmangeek@gmail.com>
2020-09-01 12:57:20 -0500

    Prototype working

--- server/server.js
+++ server/server.js
@@ -1,20 +1,21 @@
-require("dotenv").config();
-const express = require("express");
+require('dotenv').config();
+const express = require('express');
 const server = express();
-const cors = require("cors");
 const port = 3000;
-const mongoose = require("mongoose");
+const cors = require('cors');
 
+const mongoose = require('mongoose');
 mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true });
-
 const db = mongoose.connection;
-db.on("error", (error) => copnsole.error(error));
-db.once("open", () => console.log("connected to database"));
+db.on('error', (error) => console.error(error));
+db.once('open', () => console.log('Connected to database!'));
 
 server.use(express.json());
 server.use(cors());
 
-const router = require("./characters");
-server.use("/characters", router);
+const router = require('./characters');
+server.use('/characters', router);
+
+
 
 server.listen(port, () => console.log("Server running on port: ", port));
